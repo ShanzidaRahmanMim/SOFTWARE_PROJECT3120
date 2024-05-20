a7049f9b7cb23e2943ac63b74388fb1b12922316
ShanzidaRahmanMim <shanzidamim33@gmail.com>
2024-05-20 15:58:04 +0600

    Unit and UI testing Mim

---
+++ app/src/test/java/com/example/software_project_3_1/Domains/PopularDomainTest.java
@@ -0,0 +1,65 @@
+package com.example.software_project_3_1.Domains;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PopularDomainTest {
+    private PopularDomain popularDomain;
+
+    @Before
+    public void setUp() {
+        popularDomain = new PopularDomain("Beach Resort", "Maldives", "Beautiful beach resort", "King Size", true, 9.5, "beach_resort.jpg", true, 500);
+    }
+
+    @Test
+    public void testGetTitle() {
+        assertEquals("Beach Resort", popularDomain.getTitle());
+    }
+
+    @Test
+    public void testSetTitle() {
+        popularDomain.setTitle("Mountain Resort");
+        assertEquals("Mountain Resort", popularDomain.getTitle());
+    }
+
+    @Test
+    public void testGetLocation() {
+        assertEquals("Maldives", popularDomain.getLocation());
+    }
+
+    @Test
+    public void testSetLocation() {
+        popularDomain.setLocation("Switzerland");
+        assertEquals("Switzerland", popularDomain.getLocation());
+    }
+
+    @Test
+    public void testGetDescription() {
+        assertEquals("Beautiful beach resort", popularDomain.getDescription());
+    }
+
+    @Test
+    public void testSetDescription() {
+        popularDomain.setDescription("Scenic mountain resort");
+        assertEquals("Scenic mountain resort", popularDomain.getDescription());
+    }
+
+    @Test
+    public void testGetBed() {
+        assertEquals("King Size", popularDomain.getBed());
+    }
+
+    @Test
+    public void testSetBed() {
+        popularDomain.setBed("Queen Size");
+        assertEquals("Queen Size", popularDomain.getBed());
+    }
+
+    @Test
+    public void testIsGuide() {
+        assertTrue(popularDomain.isGuide());
+    }
+
+}
